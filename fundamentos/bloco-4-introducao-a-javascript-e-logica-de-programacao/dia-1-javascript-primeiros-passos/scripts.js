552ad1603de620525d9511916f60755fc95c24ab
Ludson <ludson_ps27@hotmail.com>
2022-05-12 14:53:06 -0300

    Exercicios de Tipos Primitivos

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/scripts.js
@@ -0,0 +1,13 @@
+const base = 5;
+let height = 8;
+
+console.log("Seu valor de base é: " + base);
+console.log("Seu valor de altura é: " + height);
+
+const area = base * height;
+
+console.log("A área do perimetro é: " + area);
+
+const perimeter = (2 * base) + (2 * height);
+
+console.log("A soma de todos os lados do retângulo é: " + perimeter);
