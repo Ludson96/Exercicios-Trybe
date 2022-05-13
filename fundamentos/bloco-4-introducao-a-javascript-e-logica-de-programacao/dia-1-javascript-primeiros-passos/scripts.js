99e80e83997bb128ee3f1c90edaca5b633d2d4f0
Ludson <ludson_ps27@hotmail.com>
2022-05-13 10:11:26 -0300

    Atualização

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/scripts.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/scripts.js
@@ -1,13 +1,14 @@
-const base = 5;
-let height = 8;
+var n1 = 10;
 
-console.log("Seu valor de base é: " + base);
-console.log("Seu valor de altura é: " + height);
+switch (10){
+  case (10 >=8):
+    console.log("aprovada");
+    break;
 
-const area = base * height;
+  case (n1 <=7):
+    console.log("reprovada");
+    break;
 
-console.log("A área do perimetro é: " + area);
-
-const perimeter = (2 * base) + (2 * height);
-
-console.log("A soma de todos os lados do retângulo é: " + perimeter);
+  default:
+  console.log("Não se aplica");
+}
