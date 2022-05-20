bf49f2c7baf4b65d1ae89dcd1db0b8832785a473
Ludson <ludson_ps27@hotmail.com>
2022-05-20 14:44:13 -0300

    Exercicios completos+pratica

---
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/exercicios-conteudos/script.js
@@ -0,0 +1,56 @@
+// let para = "Eu sou irmão do elementoOndeVoceEsta";
+
+// let irmao = document.createElement('section');
+// irmao.innerText = para;
+
+// pai.appendChild(irmao);
+
+// let para1 = "Eu sou filho do elementoOndeVoceEsta";
+
+// let irmao1 = document.createElement('section');
+// irmao1.innerText = para1;
+
+// elementoOndeVoceEsta.appendChild(irmao1);
+
+// let para2 = "Eu sou o segundo filho do elementoOndeVoceEsta";
+
+// let irmao2 = document.createElement('section');
+// irmao2.innerText = para2;
+// irmao2.className = 'ingredients-list-item';
+
+// elementoOndeVoceEsta.appendChild(irmao2);
+
+// let ingredientListItems = document.querySelectorAll('.ingredients-list-item');
+
+// for (let index = 0; index < ingredientListItems.length; index += 1){
+//     if (ingredientListItems[index].innerText.includes('segundo')){
+//         elementoOndeVoceEsta.removeChild(ingredientListItems[index]);
+//     }
+// }
+
+const pai = document.getElementById('pai');
+
+for (let index = pai.childNodes.length -1; index >= 0; index -= 1) {
+    console.log(pai.childNodes);
+    const currentChildren = pai.childNodes[index];
+    if (currentChildren.id !== 'elementoOndeVoceEsta') {
+        currentChildren.remove();
+    }
+}
+
+const segundoEUltimoFilhoDoFilho = document.getElementById('segundoEUltimoFilhoDoFilho');
+segundoEUltimoFilhoDoFilho.remove();
+
+console.log(pai.childNodes);
+
+// const pai = document.getElementById('pai');
+
+// for (let index = pai.childNodes.length - 1; index >= 0; index -= 1) {
+//   const currentChildren = pai.childNodes[index];
+//   if (currentChildren.id !== 'elementoOndeVoceEsta') {
+//     currentChildren.remove();
+//   }
+// }
+
+// const segundoEUltimoFilhoDoFilho = document.getElementById('segundoEUltimoFilhoDoFilho');
+// segundoEUltimoFilhoDoFilho.remove();
