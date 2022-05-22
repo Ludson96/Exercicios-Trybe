e17224e78af38b08fd6f3a7705a8cda45f5bdd65
Ludson <ludson_ps27@hotmail.com>
2022-05-22 18:23:53 -0300

    Exercicio da prática 'Administrador de tempo finalizado.'

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/exercicios-conteudos/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/exercicios-conteudos/script.js
@@ -16,3 +16,30 @@
 
 // //Conteúdos = "Parte 3 - Quqla seletor usar?"
 
+// Header
+document.getElementsByTagName('header')[0].style.backgroundColor = '#00B069';
+document.getElementsByTagName('h1')[0].style.color = 'white';
+
+//"Urgente e Importante"
+document.getElementsByTagName('div')[0].style.backgroundColor = '#FF9F84';
+document.getElementsByTagName('h3')[0].style.color = 'white';
+document.getElementsByTagName('h3')[0].style.backgroundColor = '#A500F3';
+
+//"Urgente e Não-Importante"
+document.getElementsByTagName('div')[1].style.backgroundColor = '#FF9F84';
+document.getElementsByTagName('h3')[1].style.color = 'white';
+document.getElementsByTagName('h3')[1].style.backgroundColor = '#A500F3';
+
+//"Não-Urgente e Importante"
+document.getElementsByTagName('div')[2].style.backgroundColor = '#F9DB5E';
+document.getElementsByTagName('h3')[2].style.color = 'white';
+document.getElementsByTagName('h3')[2].style.backgroundColor = '#232525';
+
+//"Não-Urgente e Não-Importante"
+document.getElementsByTagName('div')[3].style.backgroundColor = '#F9DB5E';
+document.getElementsByTagName('h3')[3].style.color = 'white';
+document.getElementsByTagName('h3')[3].style.backgroundColor = '#232525';
+
+//Footer
+document.getElementsByTagName('footer')[0].style.backgroundColor = '#003533';
+document.getElementsByTagName('footer')[0].style.color = 'white';
