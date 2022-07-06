7c2e300d0bf5b3a4f33f628f255f39aac17cdad8
Ludson <ludson_ps27@hotmail.com>
2022-07-06 11:00:42 -0300

    Exercicios feitos, mas vou refazer

---
+++ front-end/bloco-9-javascript-e-testes-assincronos/dia-1-javascript-assincrono-e-callbacks/exercicios-pratica/pratica.spec.js
@@ -0,0 +1,12 @@
+const uppercase = require('./pratica')
+
+test('Testanto exercicio 7', (done) => {
+  uppercase('test', (str) => {
+    try {
+      expect(str).toBe('TEST');
+      done();
+    } catch (error) {
+      done(error);
+    }
+  });
+});
