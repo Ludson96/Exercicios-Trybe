2c8a2e592971f2d68f28c0e0b4063cdf6426c8dd
Ludson <ludson_ps27@hotmail.com>
2022-07-10 18:23:59 -0300

    a

--- front-end/bloco-9-javascript-e-testes-assincronos/dia-2-javascript-assincrono-fetch-api-e-asyncawait/exercicio-pratica/apiScript.js
+++ front-end/bloco-9-javascript-e-testes-assincronos/dia-2-javascript-assincrono-fetch-api-e-asyncawait/exercicio-pratica/apiScript.js
@@ -1,6 +1,3 @@
-// // Utilize o HTML e o js a seguir como base: (não se esqueça de utilizar a extensão Live Server e inspecionar o console do navegador para ver os logs)
-
-// apiScript.js
 const API_URL = 'https://icanhazdadjoke.com/';
 
 const fetchJoke = () => {
@@ -9,12 +6,9 @@ const fetchJoke = () => {
     headers: { 'Accept': 'application/json' }
   };
 
-  
   fetch(API_URL, myObject)
     .then(response => response.json())
-    .then(({ joke }) => 
-    document.getElementById('jokeContainer').innerText = joke);
-  
+    .then(({ joke }) => document.getElementById('jokeContainer').innerText = joke)
 };
 
 window.onload = () => fetchJoke();
