82d22813f4ade28ec495c1ed7cf15c9ab6fa344f
Ludson <ludson_ps27@hotmail.com>
2022-07-22 14:36:10 -0300

    alteração no css

--- front-end/bloco-10-introducao-a-react/dia-2-componentes-react/exercicios-pratica/my-pokedex/src/App.js
+++ front-end/bloco-10-introducao-a-react/dia-2-componentes-react/exercicios-pratica/my-pokedex/src/App.js
@@ -6,8 +6,11 @@ import Estrutura from './Estrutura';
 class App extends React.Component {
   render() {
     return (
-      <div className='pokedex'>
-      {pokemons.map((e) => <Estrutura key={e.id} name={e.name} type={e.type} image={e.image} moreInfo={e.moreInfo} averageWeight={e.averageWeight} />)}
+      <div className='container'>
+        <h1> Pokedex </h1>
+        <div className='pokedex'>
+          {pokemons.map((e) => <Estrutura key={e.id} name={e.name} type={e.type} image={e.image} moreInfo={e.moreInfo} averageWeight={e.averageWeight} />)}
+        </div>
       </div>
     )
   }
