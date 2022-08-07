25ae861216094c55b49d497533d08e51afc7cbf6
Ludson <ludson_ps27@hotmail.com>
2022-08-07 18:31:33 -0300

    Preciso terminar os exercicios ainda

--- front-end/bloco-12-ciclo-de-vida-de-componentes-e-react-router/dia-1-ciclo-de-vida-de-componentes/exercicios-conteudo/randomuser/src/App.jsx
+++ front-end/bloco-12-ciclo-de-vida-de-componentes-e-react-router/dia-1-ciclo-de-vida-de-componentes/exercicios-conteudo/randomuser/src/App.jsx
@@ -1,48 +1,95 @@
-import './App.css';
-import React from 'react';
-// import Teste from './Components/Teste'
+// PersonDetails.js
+import React, { Component } from 'react';
+import Loading from './Components/Loading';
+import PersonCard from './Components/PersonCard';
 
-class App extends React.Component {
+class PersonDetails extends Component {
   constructor() {
-    super()
+    super();
+
+    // No construtor, criamos os nossos estados de `loading` e `person`,
+    // que vai receber o retorno da API.
+    // Começamos com o `loading` como `true`, pois queremos que ele exiba a nossa 
+    // mensagem de "carregando" enquanto não renderizamos a primeira tela.
+    // E o `person` setamos como um array vazio,
+    // pois ele irá receber o retorno da nossa API.
 
     this.state = {
-      showTimer: true,
-      picture: '',
-      name: '',
-      email: '',
-      age: '',
-    }
-
+      loading: true,
+      person: [],
+    };
   }
 
-  async componentDidMount() {
-    const response = await fetch(' https://api.randomuser.me/ ');
-    const data = await response.json();
-    const { results } = data
-    this.setState({
-      picture: results[0].picture,
-      name: results[0].name,
-      email: results[0].email,
-      age: results[0].dob.age,
-      showTimer: false,
-    })
+  // Como estudamos, o `componentDidMount` vai disparar ações após o componente
+  // ser inserido no DOM, por isso dizemos que é
+  // ideal para realizar requisições. Atribuímos ao nosso estado `person`
+  // o `data.results`, que traz os nossos dados da API.
+  // Além disso, atribuímos  o `loading` como `false`, uma vez que, quando a
+  // página é renderizada, não teremos mais a mensagem de "carregando".
+
+  componentDidMount() {
+    const url = 'https://api.randomuser.me/';
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        // Note que, nesse ponto, o nosso data retorna um objeto, e, se acessarmos
+        // o `data.results`, teremos nosso array de objetos com as informações
+        // das pessoas que são geradas aleatoriamente pela requisição.
+        // console.log(data);
+        // console.log(data.results);
+        this.setState({
+          person: data.results,
+          loading: false,
+        });
+      });
+  }
+
+  // O `shouldComponentUpdate` irá verificar se a pessoa retornada pela API tem
+  // mais de 50 anos e, com isso, irá autorizar, ou não, a atualização do componente.
+  // Caso não atualize, uma mensagem de "carregando..." será exibida na tela.
+  // Caso ocorra essa situação, dê um `console.log` no `nextState` e verifique a
+  // idade da pessoa que é trazida pela API.
+  shouldComponentUpdate(_nextProps, nextState) {
+    // console.log(nextState);
+    const AGE = 50;
+    if (nextState.person[0].dob.age > AGE) {
+      return false;
+    }
+    return true;
+  }
+
+  // Considerando que a API retorna um array de objetos,
+  // a função abaixo foi criada para extrair os dados que precisamos e
+  // atribuir esses dados a suas respectivas keys.
+  getUserElements(user) {
+    return {
+      name: `${user.name.first} ${user.name.last}`,
+      email: user.email,
+      age: user.dob.age,
+      image: user.picture.thumbnail,
+    };
   }
 
   render() {
-    const { showTimer, picture, name, email, age } = this.state;
-        
+    const { person, loading } = this.state;
+    // Condição criada para verificar o estado de `loading`. Se for `true`, irá
+    // trazer o componente de `Loading` com a mensagem de "carregando..."
+    if (loading) return <Loading />;
     return (
+      // Para renderizar as informações que precisamos, foi feito um `map` que 
+      // traz o componente de `PersonCard`, que contém as informações com nome,
+      // e-mail, idade e a foto. Passamos como props a função de `getUserElements`
+      // que retorna um objeto com as informações da pessoa e, como parâmetro da
+      // função, passamos o `currentPerson`.
       <div>
-      <div>
-        {showTimer ? <span>loading...</span> : ` picture: ${picture.large}\nName: ${name.first}\nEmail: ${email}\nAge: ${age}`}
+        {person.map((currentPerson) => (
+          <PersonCard
+            key={ currentPerson.login.uuid }
+            person={ this.getUserElements(currentPerson) }
+          />))}
       </div>
-      <main>
-        <img src={picture.large} alt="teste" />
-      </main>
-      </div>
-    )
+    );
   }
 }
 
-export default App;
+export default PersonDetails;
