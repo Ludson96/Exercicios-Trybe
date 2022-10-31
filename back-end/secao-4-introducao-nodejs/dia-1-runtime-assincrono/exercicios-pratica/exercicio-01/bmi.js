276ddee21aef7084b7855983087cb19c6ada253b
Ludson96 <ludson_ps27@hotmail.com>
2022-10-31 17:34:32 -0300

    Exercicios incompletos

---
+++ back-end/secao-4-introducao-nodejs/dia-1-runtime-assincrono/exercicios-pratica/exercicio-01/bmi.js
@@ -0,0 +1,7 @@
+// // Exercicios 1, 2, 3 e 4
+const readline = require('readline-sync');
+
+const weight = readline.questionFloat('What’ your weight? ');
+const height = readline.questionInt('What’ your height? ');
+
+console.log(`Meu weight é: ${weight} \nMeu height é: ${height}`);
