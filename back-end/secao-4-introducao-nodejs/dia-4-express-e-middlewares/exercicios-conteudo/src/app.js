503b694c575e375d183e937b950f868cb9b54982
Ludson96 <ludson_ps27@hotmail.com>
2022-11-03 09:39:50 -0300

    Exercicio conteudo 1

---
+++ back-end/secao-4-introducao-nodejs/dia-4-express-e-middlewares/exercicios-conteudo/src/app.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const validateTeam = require('./middlewares/validateTeam');
+
+const app = express();
+
+let nextId = 3;
+const teams = [
+  { id: 1, nome: 'São Paulo Futebol Clube', sigla: 'SPF' },
+  { id: 2, nome: 'Sociedade Esportiva Palmeiras', sigla: 'PAL' },
+];
+
+app.use(express.json());
+
+
+
+// ================================================================================ //
+// // 1 - 🚀 Crie um middleware existingId para garantir que o id passado como parâmetro na rota GET /teams/:id existe no objeto teams. Refatore essa rota para usar o middleware
+
+const existingId = (req, res, next) => {
+  const id = Number(req.params.id);
+  if (teams.some((t) => t.id === id)) {
+    next();
+  } else {
+    res.sendStatus(400);
+  }
+}
+// ================================================================================ //
+
+app.get('/teams', (req, res) => res.json(teams));
+
+app.get('/teams/:id', existingId, (req, res) => {
+  const id = Number(req.params.id);
+  const team = teams.find(t => t.id === id);
+    res.json(team);
+});
+
+app.post('/teams', validateTeam, (req, res) => {
+  const team = { id: nextId, ...req.body };
+  teams.push(team);
+  nextId += 1;
+  res.status(201).json(team);
+});
+
+app.put('/teams/:id', existingId, validateTeam, (req, res) => {
+  const id = Number(req.params.id);
+  const team = teams.find(t => t.id === id);
+  const index = teams.indexOf(team);
+  const updated = { id, ...req.body };
+  teams.splice(index, 1, updated);
+  res.status(201).json(updated);
+});
+
+app.delete('/teams/:id', existingId, (req, res) => {
+  const id = Number(req.params.id);
+  const team = teams.find(t => t.id === id);
+  const index = teams.indexOf(team);
+  teams.splice(index, 1);
+  res.sendStatus(204);
+});
+
+module.exports = app;
