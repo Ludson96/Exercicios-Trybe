6e84999b544ae44b11a0ffc5fad653f1ede46653
Ludson96 <ludson_ps27@hotmail.com>
2022-12-10 15:37:57 -0300

    Exercicios pratica - exercicio 6

---
+++ back-end/secao-8-introducao-typescript/dia-1-introducao-typescript/exercicio-pratica/my-ts-scripts/area.ts
@@ -0,0 +1,9 @@
+const units = ['km²', 'hm²', 'dam²', 'm²', 'dm²', 'cm²', 'mm²']
+
+export function convert (valor: number, uniB: string, uniC: string): number {
+  const uniBase = units.indexOf(uniB);
+  const uniConvert = units.indexOf(uniC);
+  const exponent = uniConvert - uniBase;
+
+  return valor * Math.pow(100, exponent);
+}
