c6c6fe872ab1ed6a836c59d2458a7ca260650592
Ludson96 <ludson_ps27@hotmail.com>
2022-12-10 15:42:01 -0300

    Exercicio pratica - exercicio 6

---
+++ back-end/secao-8-introducao-typescript/dia-1-introducao-typescript/exercicio-pratica/my-ts-scripts/volume.ts
@@ -0,0 +1,9 @@
+const units = ['mk³', 'hm³', 'dam³', 'm³', 'dm³', 'cm³', 'mm³'];
+
+export function convert (valor: number, uniB: string, uniC: string): number {
+  const uniBase = units.indexOf(uniB);
+  const uniConvert = units.indexOf(uniC);
+  const exponent = uniConvert - uniBase;
+
+  return valor * Math.pow(1000, exponent);
+}
