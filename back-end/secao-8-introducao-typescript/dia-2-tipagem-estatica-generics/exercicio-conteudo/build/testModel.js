3bb57c3be61fd358712ce6fb4495a422015d6475
Ludson96 <ludson_ps27@hotmail.com>
2022-12-13 19:31:40 -0300

    test model

--- back-end/secao-8-introducao-typescript/dia-2-tipagem-estatica-generics/exercicio-conteudo/build/testModel.js
+++ back-end/secao-8-introducao-typescript/dia-2-tipagem-estatica-generics/exercicio-conteudo/build/testModel.js
@@ -4,8 +4,13 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const BookModel_1 = __importDefault(require("./database/models/BookModel"));
+const CommentModel_1 = __importDefault(require("./database/models/CommentModel"));
 (async () => {
     const books = await BookModel_1.default.findAll({ raw: true });
     console.table(books);
+    const comments = await CommentModel_1.default.findAll({ raw: true });
+    console.table(comments);
+    const booksWithComments = await BookModel_1.default.findAll({ raw: true, include: ['comments'] });
+    console.table(booksWithComments);
     process.exit(0);
 })();
--- back-end/secao-8-introducao-typescript/dia-2-tipagem-estatica-generics/exercicio-conteudo/src/testModel.ts
+++ back-end/secao-8-introducao-typescript/dia-2-tipagem-estatica-generics/exercicio-conteudo/src/testModel.ts
@@ -1,7 +1,16 @@
-import Books from './database/models/BookModel';
+import Books from './database/models/BookModel'
+import Comments from './database/models/CommentModel';
 
 (async () => {
+
   const books = await Books.findAll({ raw: true });
   console.table(books);
+
+  const comments = await Comments.findAll({ raw: true });
+  console.table(comments);
+
+  const booksWithComments = await Books.findAll({ raw: true, include: ['comments'] });
+  console.table(booksWithComments);
   process.exit(0);
+
 })();
