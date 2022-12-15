9f82568c1013d826206a6878e1e00c62239e8b19
Ludson96 <ludson_ps27@hotmail.com>
2022-12-15 18:49:59 -0300

    create model: author

---
+++ back-end/secao-8-introducao-typescript/dia-2-tipagem-estatica-generics/exercicio-pratica/exercise-sequelize/src/database/models/Author.ts
@@ -0,0 +1,26 @@
+import { Model, INTEGER, STRING } from 'sequelize';
+import db from '.'
+
+class Author extends Model {
+  declare id: number;
+  declare name: string;
+}
+
+Author.init({
+  id: {
+    type: INTEGER,
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true, 
+  },
+  name: {
+    type: STRING,
+    allowNull: false,
+  },
+}, {
+  sequelize: db,
+  modelName: 'authors',
+  timestamps: false,
+});
+
+export default Author;
