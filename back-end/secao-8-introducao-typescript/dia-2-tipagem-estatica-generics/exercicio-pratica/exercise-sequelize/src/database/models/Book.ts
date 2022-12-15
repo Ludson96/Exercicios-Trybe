ead8d832dd0f58a805d8c7962a1a7df68ce524a6
Ludson96 <ludson_ps27@hotmail.com>
2022-12-15 18:53:38 -0300

    create model: Book

---
+++ back-end/secao-8-introducao-typescript/dia-2-tipagem-estatica-generics/exercicio-pratica/exercise-sequelize/src/database/models/Book.ts
@@ -0,0 +1,50 @@
+import { Model, INTEGER, STRING } from 'sequelize';
+import db from '.'
+
+class Book extends Model {
+  declare id: number;
+  declare title: string;
+  declare authorId: number;
+  declare genreId:number;
+}
+
+Book.init({
+  id: {
+    type: INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  title: {
+    type: STRING,
+    allowNull:true,
+  },
+  authorId: {
+    type: INTEGER,
+    allowNull:true,
+    onUpdate: 'CASCADE',
+    onDelete: 'CASCADE',
+    field: 'author_id',
+    references: {
+      model: 'authors',
+      key: 'id'
+    },
+  },
+  genreId: {
+    type: INTEGER,
+    allowNull: true,
+    onUpdate: 'CASCADE',
+    onDelete: 'CASCADE',
+    field: 'genre_id',
+    references: {
+      model: 'genres',
+      key: 'id',
+    },
+  },
+}, {
+  sequelize: db,
+  modelName: 'books',
+  timestamps: false,
+});
+
+export default Book;
