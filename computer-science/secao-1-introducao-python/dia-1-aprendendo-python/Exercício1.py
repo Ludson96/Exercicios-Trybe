7d01d0ca40b56a613c675e6f04c34906d8ec8cfa
Ludson96 <ludson_ps27@hotmail.com>
2023-03-21 19:09:51 -0300

    feat: :sparkles: Exercicios pratica

---
+++ computer-science/secao-1-introducao-python/dia-1-aprendendo-python/Exercício1.py
@@ -0,0 +1,32 @@
+def maior1(x, y):
+    if x > y:
+        return x
+    return y
+
+
+def media2(lists):
+    new_list = 0
+    for list in lists:
+        new_list += list
+    return new_list / len(lists)
+
+
+def asteristico3(n):
+    for linha in range(n):
+        return n * "*"
+
+
+def maior_caracteres4(listas):
+    print(max(listas, key=len))
+
+
+def paint_costs(area):
+    can_price = 80
+    required_liters = area / 3
+    required_cans = required_liters // 18
+    if required_liters % 18:
+        required_cans += 1
+    return required_cans, required_cans * can_price
+
+
+print(paint_costs(100))
