e9bc345041afa50c01bc592c3b96e2a3da55c415
Ludson96 <ludson_ps27@hotmail.com>
2023-03-27 14:51:41 -0300

    feat: :sparkles: Exercicio 3

---
+++ computer-science/secao-1-introducao-python/dia-2-entrada-saida-dados/exercicios-pratica/exercicio3.py
@@ -0,0 +1,23 @@
+import random
+
+with open("./words.txt", "r") as file:
+    words = [line.strip() for line in file.readlines()]
+
+word = random.choice(words)
+
+scrambled_word = "".join(random.sample(word, len(word)))
+
+tentativas = 3
+
+while tentativas > 0:
+    resposta = input(f"{scrambled_word}  qual é a palavra? ")
+
+    if resposta == word:
+        print("Parabéns, você acertou")
+        break
+    else:
+        tentativas -= 1
+    if tentativas == 0:
+        print(f"Suas tentativas acabaram. Fim de jogo! A palavra era {word}")
+    else:
+        print(f"Resposta errada você tem {tentativas} tentativas")
